dacdd3250486e18c5baf48e94f163280f911018d
unknown <Sena@MEDAPRCSGFSD729.FormacionSena.RED>
2024-03-11 15:16:49 -0500

    cambio en schema users

--- models/user.js
+++ models/user.js
@@ -2,18 +2,32 @@ const mongoose = require("mongoose");
 
 // Define el esquema del usuario
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  age: {
-    type: Number,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  }
+    name: { // Nombre
+        type: String,
+        required: true
+    },
+    phone: { // Teléfono
+        type: String,
+        required: true
+    },
+    document: { // Documento
+        type: String,
+        required: true
+    },
+    email: { // Correo
+        type: String,
+        required: true,
+        unique: true // Asegura que el correo sea único en la base de datos
+    },
+    username: { // Usuario
+        type: String,
+        required: true,
+        unique: true // Asegura que el nombre de usuario sea único
+    },
+    password: { // Contraseña
+        type: String,
+        required: true
+    }
 });
 
 // Crea el modelo 'User' a partir del esquema definido
