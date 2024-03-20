40dce45e8d2c10631c4a0137c4e2061a76fa1fb8
unknown <Sena@MEDAPRCSGFSD729.FormacionSena.RED>
2024-03-20 14:59:34 -0500

    request

--- models/order.js
+++ models/order.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 // Define el esquema del usuario
 const orderSchema = new mongoose.Schema({
+    user_id: {
+        type: String,
+        required: true
+    },
     creation_date: {
         type: String,
         required: true
